fb603f5d9b775e8dec6b90b091dabd0cd302ca73
Official Bika <phoetaay@gmail.com>
2026-03-20 00:01:48 +0700

    Update streamer.py

--- app/services/streamer.py
+++ app/services/streamer.py
@@ -1,1 +1,12 @@
+from ntgcalls import AudioQuality, MediumQualityAudio
+from pytgcalls.types.input_stream import AudioPiped
+from app.core.tgcalls_client import call_py
 
+async def start_stream(chat_id: int, stream_url: str):
+    await call_py.play(
+        chat_id,
+        AudioPiped(
+            stream_url,
+            audio_parameters=MediumQualityAudio(),
+        ),
+    )
