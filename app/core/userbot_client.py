aad8a0424b33f106d5290471c04ad1bface997db
Official Bika <phoetaay@gmail.com>
2026-03-19 23:59:59 +0700

    Update userbot_client.py

--- app/core/userbot_client.py
+++ app/core/userbot_client.py
@@ -1,1 +1,9 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, SESSION_STRING
 
+assistant = Client(
+    "bika_music_assistant",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION_STRING,
+)
